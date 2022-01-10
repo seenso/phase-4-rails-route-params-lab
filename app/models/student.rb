a282b0f74173056ad406059af48195ec9520d984
seenso <>
2022-01-10 08:41:47 -0900

    Completed assignment - w Bonus

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,15 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  #BONUS
+   # class method written using a scope (https://www.rubyguides.com/2019/10/scopes-in-ruby-on-rails/)
+   scope :by_name, ->(name) { 
+     where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") 
+    }
+  
+   # class method written without using a scope
+   # def self.by_name(name)
+   #   Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+   # end
+
 end
